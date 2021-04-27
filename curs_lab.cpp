485e790b9b56ad595772a2c6d286079b6d77235e
Yaroslav <78160604+YaroslavYaryk@users.noreply.github.com>
2021-04-27 13:55:45 +0300

    made some changes

--- curs_lab.cpp
+++ curs_lab.cpp
@@ -2,15 +2,22 @@
 #include <cstring>
 #include <ctime>
 #include <cstdlib>
+#include <chrono>
 using namespace std;
+using namespace chrono;
+
 #define ROW 5
 #define COL 5
 
 int valuable_integers[] = {11,12,13,14,15,21,22,23,24,25,31,32,33,34,35,41,42,43,44,45,51,52,53,54,55};
 
+void print_start();
+void print_again();
+
+
+
 class Treasure_box{
         friend istream &operator >>(istream &stream, Treasure_box &obj);
-
 private:
     int box[ROW][COL] = {{34,21,32,41,25},  
                      {14,42,43,14,31},
@@ -41,7 +48,7 @@ private:
         int i;
         bool f; 
         f = false;
-        for (i=0; i<10; i++)
+        for (i=0; i<25; i++)
             if (arr[i]==elem)
                 f = true;
         return f;        
@@ -75,28 +82,39 @@ public:
         int start = box[0][0];
         int des_pos = (int)start/10;
         int od_pos = start%10;
+        int count = 0;
         if (operation == "work"){
+            if (box[des_pos-1][od_pos-1] == 11){
+                printa(0, 0);
+                cout << ("found it  11 ") ;
+                cout << endl <<"make 1 step" <<endl;
+                return true;
+            }
             printa(0, 0);
             printa(des_pos-1, od_pos-1);
+            count +=1;
+
+    
         }
-        int count = 0;
         while (box[des_pos-1][od_pos-1] != des_pos*10 + od_pos ){
             start = box[des_pos-1][od_pos-1];
             des_pos = (int)start/10;
             od_pos = start%10;
+            count+=1;
             if (operation == "check"){
-                count+=1;
-                if (count == 100)
+                if (count == 1000)
                     return false;
             }        
-            else if (operation == "work")       
+            else if (operation == "work")  { 
                 printa( des_pos-1, od_pos-1);
+            } 
         }        
         
         if (operation == "check")
             return true ;          
         cout << ("found it ") ;
         cout << (box[des_pos-1][od_pos-1]);  
+        cout << endl <<"make " << count << " steps" <<endl;
 
     }
     void printer(){
@@ -155,35 +173,72 @@ istream &operator >>(istream &stream, Treasure_box &obj){
 
 
 int main(){
+    auto begin = steady_clock::now();
     Treasure_box obj;
     string choice;
-    cout << "Wanna start? ";
-    cin >> choice;
-    if (choice == "yes" || choice == "Yes" || choice == "1"){
-        cout << "which one you wanna add element?\n1)On your own\n2)random\n3)default matrix" << endl;
+    string choicen;
+    do{
+        print_start();
         string result;
         cin >> result;
-        if (result == "1" || result == "first")
+        if (result == "1" )
             cin >> obj;
-        else if (result == "2" || result == "second"){
+        else if (result == "2"){
             obj.randoma();
         }
-        else if (result == "3" || result == "third"){
+        else if (result == "3" ){} 
+
+        if (result == "1" || result == "2"|| result == "3"  ){
+            auto begin = steady_clock::now();   
+            if (obj.is_true() && obj.find_special_element("check")){
+                obj.find_special_element();
+            }
+            else{
+                obj.printer();
+                cout << endl <<"sorry but  can't find treasure element" << endl;
+            }
+            auto end = steady_clock::now();
+            auto elapsed_ms = duration_cast<milliseconds>(end - begin);
+            cout <<  "| time is : " <<  elapsed_ms.count() << "ms |\n";
+        }
+
+        else{
+            cout << "Sorry but you entered the wrong answer " << endl;
+            cout << "If you want you can try again " << endl;
 
         }    
+        print_again();
+        cin >> choicen;
 
-        if (obj.is_true() && obj.find_special_element("check")){
-            obj.find_special_element();
-        }
-        
-        else{
-            cout << "sorry but  can't find treasure element" << endl;
-            obj.printer();
-        }
-    }
-    else{
-        cout << "good luck" << endl;
-    }
-    return 0;
+	} while (choicen=="again" || choicen=="1" || choicen=="first");    
 
+    cout << "That was  pleasure to work with you, good luck, buddy" << endl;
+    auto end = steady_clock::now();
+    auto elapsed_ms = duration_cast<milliseconds>(end - begin);
+    cout << "| total time is : " << elapsed_ms.count() << "ms |" << endl;
 }
+
+void print_start(){
+    string tabs = " ---------------------------------"; 
+    cout << " _________________________________" << endl;
+    cout << "| which one you wanna add element |" << endl;
+    cout << tabs << endl;
+    cout << "| 1) On your own                  |" <<endl;
+    cout << tabs << endl;
+    cout << "| 2) Random matrix                |" <<endl;
+    cout << tabs << endl;
+    cout << "| 3) Default one                  |" <<endl;
+    cout << tabs << endl;
+}
+
+void print_again(){
+    string tabs = " ------------------------"; 
+    cout << " ________________________" << endl;
+    cout << "| What should we do now? |" <<endl;
+    cout << tabs << endl;
+    cout << "| 1) Play one more time  |" <<endl;
+    cout << tabs << endl;
+    cout << "| 2) just quit           |" <<endl;
+    cout << tabs << endl;
+}
+
