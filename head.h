a96228553512aaf3e291830bf483fbf4e8fa67fd
Yaroslav <78160604+YaroslavYaryk@users.noreply.github.com>
2021-05-21 16:41:09 +0300

    Final Final project

---
+++ head.h
@@ -0,0 +1,145 @@
+#include "head_head.h"
+
+class Treasure_box : public Storage{ //main class
+    friend void input(Treasure_box & obj); //friend function of filling arrat
+private:
+    int box[ROW][COL]; //our private array
+
+public:
+    Treasure_box(){
+        for(int i = 0; i< ROW; i++){
+            for(int j = 0; j < COL; j++){
+                this->box[i][j] = 0; //fill zero
+            }
+        }
+    }
+
+    bool is_true(){ //fonction checks if it is treasure
+        for (int i = 0; i < ROW; i++){   
+            for (int j = 0; j <COL; j++){
+                if (box[i][j] == (i+1)*10 + (j+1)){
+                    return true; //if it is 
+                }  
+            }      
+        }
+        //otherwise 
+        printer(box); //print array
+        return false;//return false
+    }
+
+
+    void set_default(){ //fill our matrix by default elements
+        int index = 0;//as it's said in the example
+        for(int i = 0; i< ROW; i++){
+            for(int j = 0; j < COL; j++){
+                this->box[i][j] = defaulter[index]; //array default elements
+                index+=1;
+            }
+        }
+    }
+
+
+    void randoma(){//filling random elements
+        srand(time(NULL)); //generate of random symbols
+        for (int i=0; i< ROW; i++){
+            for (int j = 0; j <COL; j++){
+                int elem  = rand() % 25; // from 0 to 25 
+                this -> box[i][j] = valuable_integers[elem]; //get  element from  
+            } //array of valuable integers
+        }
+    }
+
+
+    //function can work in two modes (work and check)
+    //if it's check wy try to find element if it's 
+    //possible - return true
+    //if "work" it show all way
+    bool find_special_element(string operation = "work"){ // main function
+        int start = box[0][0]; //firts element 
+        int des_pos = (int)start/10;//get decimal position
+        int od_pos = start%10; //get single position
+        int count = 0; //counter
+
+        if (operation == "work"){ 
+            if (box[des_pos-1][od_pos-1] == 11){ //if first elem is treasure
+                printa(box,0, 0); //print array
+                cout << ("\x1B[33mfound it  11 ") ;
+                cout << endl <<"make 1 step\033[0m" <<endl; //count of steps
+                return true; //return true
+            }
+            //else
+            printa(box,0, 0); //print first step
+            printa(box,des_pos-1, od_pos-1); //print second steo
+            count +=1; //add 1 to couner
+    
+        }
+        while (box[des_pos-1][od_pos-1] != des_pos*10 + od_pos ){ //while it's not treasure
+            start = box[des_pos-1][od_pos-1]; //get element to 'new' start
+            des_pos = (int)start/10;//get decimal position
+            od_pos = start%10;//get single position
+            count+=1;//add 1 to couner
+            if (operation == "check"){
+                if (count == 26){ // if count is 26 (max possible can be 25) so we cnt 
+                //fint it anyway
+                    printer(box); //print array
+                    return false;
+                }
+            }        
+            else if (operation == "work")  { //if it's work
+                printa(box, des_pos-1, od_pos-1); // select this step
+            } 
+        } 
+
+        //we found theasure
+        if (operation == "check")
+            return true ;  //return   
+        //else     
+        cout << "\x1B[33mFound it ";
+        cout << (box[des_pos-1][od_pos-1]);  //select treause
+        cout << endl <<"make " << count+1 << " steps\033[0m" <<endl;//show count of steps
+        return true;
+    }
+
+    ~Treasure_box(){
+        for(int i = 0; i< ROW; i++){
+            for(int j = 0; j < COL; j++){
+                this->box[i][j] = '\0'; //fill empty
+            }
+        }
+    }
+
+};
+
+
+void input(Treasure_box & obj){ //function of filling array 
+    cout << "make sure that element you're entering bigger than 11 and lowwer then 55"<< endl;
+    for (int i = 0; i< 5 ; i++){ //go througt array
+        for (int j = 0; j <5; j++){//...
+            int elem;
+            while (true){ //in order to catch the mistake of input
+                cout << "Box[" << i+1 << "]["<< j+1<< "]"; 
+                try{
+                    cin >> elem; //try to input element
+                    if (cin){ //check if we entered exactly integer
+                        if (obj.check_in_array(valuable_integers, elem) ){ //if it's appropiarite integer
+                            obj.box[i][j] = (int)elem; //we add it to our array
+                            break;
+                        }
+                        else{
+                            throw 505 ; //if it's not in valuable integer throw an error
+                        }
+                    }
+                    else{
+                        throw 505 ; //if it's not ineger
+                    } 
+                }    
+                catch(int n){
+                    cout << "\a\x1B[31mwrong type of element, try again\033[0m" << endl; //try again
+                    cin.clear();//clear buffer
+                    fflush(stdin);
+                }
+            }
+        }
+    }
+}
+
---
+++ head_head.h
@@ -0,0 +1,106 @@
+#include <iostream>
+#include <cstring>
+#include <cstdlib>//include libraries
+#include <chrono>
+
+using namespace std;
+#define ROW 5 //num of rows
+#define COL 5//num of cols
+
+int defaulter[] = {34,21,32,41,25,14,42,43,14,31,54,45,52,42,23,33,15,51,31,35,21,52,33,13,23};
+//default array of integers
+int valuable_integers[] = {11,12,13,14,15,21,22,23,24,25,31,32,33,34,35,41,42,43,44,45,51,52,53,54,55};
+//integers that we can use(appropiarite to aur array)
+class Storage{
+public:
+    
+    void printa(int box[ROW][COL],int des, int od){ //print array and element
+        cout <<("\n\x1B[36m+----++----++----++----++----+\n");//use different color  
+        for (int i = 0; i <ROW; i++){ //go through array
+            for (int j = 0; j <COL; j++){
+                if (i == des && j == od){ //if value == row*10 + col 
+                    cout << "|\033[0m" << "\x1B[33m "<< box[i][j] <<" \033[0m" << "\x1B[36m|";
+                    continue;//select it
+                }    
+                else
+                    cout << "| " <<box[i][j] <<" |" ; //shoe other element
+            }        
+            cout <<("\n+----++----++----++----++----+\n");
+        }
+        cout <<"\033[0m";
+    }
+
+
+    void printer(int box[ROW][COL]){ //show only array
+        cout <<("\n+----++----++----++----++----+\n");    
+        for (int i = 0; i <ROW; i++){ //go though array
+            for (int j = 0; j <COL; j++){
+                cout << "| " <<box[i][j] <<" |" ;//show each element
+            }        
+            cout <<("\n+----++----++----++----++----+\n");
+        }
+    }
+
+    //check if element is in some array
+    bool check_in_array(int arr[25], int elem){ 
+        int i;
+        bool f; 
+        f = false;
+        for (i=0; i<25; i++) //go though array
+            if (arr[i]==elem) //if elemnt one of it
+                f = true; // return true
+        return f;        //otherwise return false
+    }
+};
+    
+void print_start(){//start menu
+    string tabs = " ---------------------------------"; 
+    cout << " _________________________________" << endl;
+    cout << "| which one you wanna add element |" << endl;
+    cout << tabs << endl; 
+    cout << "| 1) On your own                  |" <<endl;
+    cout << tabs << endl; 
+    cout << "| 2) Random matrix                |" <<endl;
+    cout << tabs << endl; 
+    cout << "| 3) Default one                  |" <<endl;
+    cout << tabs << endl; 
+    cout << "| 4) quit                         |" <<endl;
+    cout << tabs << endl; 
+}
+
+
+void print_again(){ //ask try again
+    string tabs = " ------------------------"; 
+    cout << " ________________________" << endl;
+    cout << "| What should we do now? |" <<endl;
+    cout << tabs << endl;
+    cout << "| 1) Play one more time  |" <<endl;
+    cout << tabs << endl;
+    cout << "| 2) just quit           |" <<endl;
+    cout << tabs << endl;
+}
+
+int get_result_to_try_again(int choicen){
+     while(true){//try again
+        print_again();
+        try{
+            cin >> choicen; //continue playing or quit
+            if (cin){ //check if it's integer
+                if (choicen == 1 || choicen == 2) // 1 or 2
+                    return choicen; //we entered correct answer (up to choice)  
+                else{//not 1 or 2
+                    throw 505; //error
+                }    
+            }
+            else{//not integer
+                throw 505;//error
+            }
+        }
+        catch(int i){ //except errors
+            cout << "\a\x1B[31mWrong answer\033[0m" << endl;
+            cin.clear(); //clear buffer
+            fflush(stdin);
+        }
+        return 0;
+    }    
+}
